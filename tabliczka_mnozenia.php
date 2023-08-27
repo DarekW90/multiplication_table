963c89460becbca6ff10b0e6db9931c73e4fdf77
Dariusz Walda <walda.darek@gmail.com>
2023-08-27 19:15:35 +0200

    cleaning code

--- tabliczka_mnozenia.php
+++ tabliczka_mnozenia.php
@@ -2,72 +2,69 @@
     
     $startTime = time();
     $programStart = microtime(true);
-    $inFileNameSize = $_GET['size'];
 
-    // tworzenie pliku cache i ustawienie jego lifetime
-    $cacheFile = "multiply_table_cache_".$inFileNameSize.".php";
-    $cacheTime = 10; // lifetime pliku cache
+    // super globalna $_GET pobiera "size" z adresu URL strony
+    $size = isset($_GET['size']) ? $_GET['size'] : '';
 
-    if (file_exists($cacheFile) && (filemtime($cacheFile) + $cacheTime > time()))
-    {
-        echo "Cache loaded!<br><br>";
-        readfile($cacheFile);
+    // Sprawdzenie czy parametr "size" jest ustawiony i jest liczbą całkowitą
+    if (ctype_digit($size) && $size >= 1 && $size <= 100) {
+        echo "<b>'Size' pobrany z URL :</b> " . $size . "<br><br>";
+        
+        // Tworzenie pliku cache i ustawienie jego lifetime
+        $cacheFile = "multiply_table_cache_size_" . $size . ".txt";
+        $cacheTime = 10; // lifetime pliku cache (w sekundach)
+
+        // Sprawdzenie czy plik cache istnieje i czy nie jest przestarzały
+        if (file_exists($cacheFile) && (filemtime($cacheFile) + $cacheTime > time()))
+        {
+            readfile($cacheFile);
+            $programStop = microtime(true);
+            $diffTime = (($programStop - $programStart) * 1000);
+            echo "<h2>Różnica: " . round($diffTime, 4) . " ms.</h2>";
+            echo "Cache loaded!";
+            exit();
+        }
+            
+        ob_start();
+
+        // Start programu
+        $startDate = date("H:i:s");
+        echo "START " . $startDate . "<br><br>";
+
+        function multiplyTable($size) {
+            // Tworzenie tabliczki mnożenia
+            $result = array();
+            for ($i = 1; $i <= $size; $i++) {
+                $row = array();
+                for ($j = 1; $j <= $size; $j++) {
+                    $row[$j] = $i * $j;
+                }
+                $result[$i] = $row;
+            }
+
+            return json_encode($result, JSON_UNESCAPED_UNICODE);
+        }
+
+        // Wywołanie funkcji i wyświetlenie wyniku
+        $result = multiplyTable($size);
+        echo $result;
+
+        // Zapis pliku cache
+        $file = fopen($cacheFile, "w");
+        if (fwrite($file, ob_get_contents())) {
+            fclose($file);
+            echo "<br>Cache file saved successfully.";
+        } else {
+            echo "<br>Error writing to cache file.";
+        }
+
+        // Stop programu
         $programStop = microtime(true);
         $diffTime = (($programStop - $programStart) * 1000);
-        echo "<h2>Różnica: " . $diffTime . " ms.</h2>";
-        exit();
+        echo "<h2>Różnica czasu: " . round($diffTime, 4) . " ms.</h2>";
+        echo "New cache file saved!";
+    } else {
+        echo "<b>Parametr 'size' nie został ustawiony w adresie URL lub jest niepoprawny.</b><br><br> Należy dopisać w adresie URL - ?size=(1-100)";
     }
 
-    ob_start();
-
-    //start programu
-
-    $startDate = date("H:i:s");
-    echo "START " . $startDate . "<br>";
-
-    // funkcja budująca tabliczkę mnożenia JSON w oparciu o "size" podanym w URL strony
-    function multiply_table($size) {
-        if (!is_numeric($size) || $size < 1 || $size > 1000) {
-            return json_encode(array("error" => "Wymiar tablicy musi być liczbą od 1 do 10."), JSON_UNESCAPED_UNICODE); // JSON_UNESCAPED_UNICODE umożliwia wyświetlenie polskich znaków w przypadku błędu podania wartości "size" poza wymaganą
-        }
-        // pętla tworząca tabliczkę mnożenia
-        $result = array();
-        for ($i = 1; $i <= $size; $i++) {
-            $row = array();
-            for ($j = 1; $j <= $size; $j++) {
-                $row[$j] = $i * $j;
-            }
-            $result[$i] = $row;
-        }
-
-        return json_encode($result);
-        
-    }
-    // super globalna $_GET pobiera "size" z URL strony
-    // ? parametr trójargumentowy oddziela isset (gdy 'size' jest ustawiony) od intval (gdy parametr "size" nie istnieje) ustawiając go automatycznie na 0
-    $size = isset($_GET['size']) ? intval($_GET['size']) : 0;
-
-    // potwierdzenie czy parametr "size" jest ustawiony czy nie
-    if ($size === 0) {
-        echo "<b>Parametr 'size' nie został ustawiony w adresie URL.</b><br><br>";
-    }
-    else {
-        echo "<b>'Size' pobrany z URL :</b> " . $_GET['size'] . "<br><br>";
-    }
-    
-    // wywoływanie rezultatu funkcji
-    $result = multiply_table($size);
-    echo $result;
-
-    $file = fopen($cacheFile, "w");
-    fwrite($file,ob_get_contents());
-    fclose($file);
-
-
-    //program stop
-    $endDate = date("H:i:s");
-    $programStop = microtime(true);
-    $diffTime = (($programStop - $programStart) * 1000);
-    echo "<h2>Różnica: " . $diffTime . " ms.</h2>";
-    echo "Saving new cache"
 ?>
